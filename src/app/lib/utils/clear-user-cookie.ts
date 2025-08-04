b40f671bf3580b6a2027ff9dc76ee67940380989
nesamouzehkesh <n.mouzehkesh@gmail.com>
2025-08-04 14:56:25 +1000

    Post submission commit: Clean up

--- src/app/lib/utils/clear-user-cookie.ts
+++
@@ -1,6 +0,0 @@
-import { USER_INFO_COOKIE_KEY } from './constants';
-
-export function clearUserCookie(): void {
-  // Clear the user cookie by setting it to expire in the past
-  document.cookie = `${USER_INFO_COOKIE_KEY}=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;`;
-} 
